428486975eab792a7f59d4db69e9adf029bbc0c3
alsu <alsukuchukova@gmail.com>
2025-05-27 14:57:13 +0300

    Initial commit

--- src/texteditor.py
+++ src/texteditor.py
@@ -7,7 +7,7 @@ filename = None
 def newFile():
     global filename
     filename = "Untitled"
-    text.delete(1.0, END)  
+    text.delete(1.0, END) 
     
 def saveFile():
     global filename
@@ -16,7 +16,7 @@ def saveFile():
         return
     
     try:
-        with open(filename, 'w', encoding='utf-8') as f: 
+        with open(filename, 'w', encoding='utf-8') as f:  
             f.write(text.get(1.0, END))
     except:
         showerror(title="Oops!", message="Unable to save file...")
@@ -27,7 +27,7 @@ def saveAs():
     if not f: 
         return
     
-    filename = f 
+    filename = f  
     try:
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(text.get(1.0, END))
@@ -36,8 +36,8 @@ def saveAs():
 
 def openFile():
     global filename
-    f = askopenfilename()  
-    if not f:  
+    f = askopenfilename() 
+    if not f: 
         return
     
     try:
@@ -48,23 +48,43 @@ def openFile():
     except:
         showerror(title="Oops!", message="Unable to open file...")
 
+def changeSelectedTextColor(color):
+    try:
+        if text.tag_ranges(SEL):
+            sel_start = text.index(SEL_FIRST)
+            sel_end = text.index(SEL_LAST)
+            text.tag_add("colored", sel_start, sel_end)
+            text.tag_config("colored", foreground=color)
+    except:
+        pass 
+
 root = Tk()
-root.title("Python text editor")
+root.title("Text Editor")
 root.minsize(width=400, height=400)
 root.maxsize(width=400, height=400)
 
-text = Text(root, width=400, height=400)
+text = Text(root, width=400, height=400, fg="black")
 text.pack()
 
 menubar = Menu(root)
+
+# Меню File
 filemenu = Menu(menubar)
 filemenu.add_command(label="New", command=newFile)
 filemenu.add_command(label="Open", command=openFile)
 filemenu.add_command(label="Save", command=saveFile)
 filemenu.add_command(label="Save As", command=saveAs)
 filemenu.add_separator()
-filemenu.add_command(label="Quit", command=root.quit)
+filemenu.add_command(label="Quit", command=root.destroy)
 menubar.add_cascade(label="File", menu=filemenu)
+
+# Меню Сolor для изменения цвета выделенного текста
+colormenu = Menu(menubar)
+colormenu.add_command(label="Black", command=lambda: changeSelectedTextColor("black"))
+colormenu.add_command(label="Red", command=lambda: changeSelectedTextColor("red"))
+colormenu.add_command(label="Blue", command=lambda: changeSelectedTextColor("blue"))
+colormenu.add_command(label="Green", command=lambda: changeSelectedTextColor("green"))
+menubar.add_cascade(label="Color", menu=colormenu)
 
 root.config(menu=menubar)
 root.mainloop()
