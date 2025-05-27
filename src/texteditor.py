8d2995acdb454930c5b195915d1b28fecd1047b2
alsu <alsukuchukova@gmail.com>
2025-05-27 10:23:37 +0300

    Initial commit

--- src/texteditor.py
+++ src/texteditor.py
@@ -7,27 +7,27 @@ filename = None
 def newFile():
     global filename
     filename = "Untitled"
-    text.delete(1.0, END)  # Исправлено с 0.0 на 1.0 для Python 3
+    text.delete(1.0, END)  
     
 def saveFile():
     global filename
-    if filename == "Untitled" or filename is None:  # Если файл новый
+    if filename == "Untitled" or filename is None:  
         saveAs()
         return
     
     try:
-        with open(filename, 'w', encoding='utf-8') as f:  # Добавлена кодировка
+        with open(filename, 'w', encoding='utf-8') as f: 
             f.write(text.get(1.0, END))
     except:
         showerror(title="Oops!", message="Unable to save file...")
 
 def saveAs():
     global filename
-    f = asksaveasfilename(defaultextension='.txt')  # Используем asksaveasfilename вместо asksaveasfile
-    if not f:  # Если пользователь отменил
+    f = asksaveasfilename(defaultextension='.txt') 
+    if not f: 
         return
     
-    filename = f  # Сохраняем новое имя файла
+    filename = f 
     try:
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(text.get(1.0, END))
@@ -36,15 +36,15 @@ def saveAs():
 
 def openFile():
     global filename
-    f = askopenfilename()  # Используем askopenfilename вместо askopenfile
-    if not f:  # Если пользователь отменил
+    f = askopenfilename()  
+    if not f:  
         return
     
     try:
         with open(f, 'r', encoding='utf-8') as file:
             text.delete(1.0, END)
             text.insert(1.0, file.read())
-        filename = f  # Сохраняем имя открытого файла
+        filename = f  
     except:
         showerror(title="Oops!", message="Unable to open file...")
 
